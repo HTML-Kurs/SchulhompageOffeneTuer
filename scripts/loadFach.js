30aa183dd776c1a4387ef274d39ae56b27a06275
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2021-01-27 21:32:59 +0100

    Technik Update

--- scripts/loadFach.js
+++ scripts/loadFach.js
@@ -3,6 +3,6 @@ const fach = (new URLSearchParams( window.location.search)).get("fach");
 const Text = loadData("./data/faecher/" + fach + "/text.txt");
 console.log(Auswahl_Template);
 var Output =  "";
-Output = Auswahl_Template.replace(/\[NAME\]/g, fach);
+Output = Auswahl_Template.replace(/\[NAME\]/g, fach.replace(/[0-9]/g, '').trim());
 Output = Output.replace(/\[TEXT\]/g, Text);
 document.write(Output);
