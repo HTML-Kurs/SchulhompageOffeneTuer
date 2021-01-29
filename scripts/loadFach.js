b6270e35d60df6d647f73daddd3e71da84e471ab
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2021-01-29 09:58:17 +0100

    u

--- scripts/loadFach.js
+++ scripts/loadFach.js
@@ -1,8 +1,8 @@
 const Auswahl_Template = loadData("./html_templates/Fach.html");
 const fach = (new URLSearchParams( window.location.search)).get("fach");
-const Text = loadData("./data/faecher/" + fach + "/text.txt");
-console.log(Auswahl_Template);
+const Text = loadData("./data/fächer/" + fach + "/text.txt");
 var Output =  "";
-Output = Auswahl_Template.replace(/\[NAME\]/g, fach.replace(/[0-9]/g, '').trim());
+Output = Auswahl_Template.replace(/\[NAME\]/g, fach);
 Output = Output.replace(/\[TEXT\]/g, Text);
 document.write(Output);
+
