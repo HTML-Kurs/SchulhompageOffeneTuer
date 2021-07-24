aedd950b3176a244d28b9e7db08e2f3862bdeb9f
JustforStephan231 <76181833+JustforStephan231@users.noreply.github.com>
2021-07-24 12:48:33 +0200

    Create animationFAQs.js

---
+++ scripts/animationFAQs.js
@@ -0,0 +1,18 @@
+var numberFAQ = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+// this function add an text to a FAQ-question, if it was clicked
+function change(arrayPosition, name, text){
+  const x = document.getElementById(name);
+  
+  if(numberFAQ[arrayPosition] == 0){
+    x.innerHTML = text;
+    numberFAQ[arrayPosition]++;
+  }
+  else{
+    x.innerHTML = "";
+    numberFAQ[arrayPosition] = 0;
+  }
+}
+function findFAQ(){
+  let msg = document.querySelector("#content").value;
+  window.alert(x);
+}
